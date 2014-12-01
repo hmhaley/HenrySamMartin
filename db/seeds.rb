92cf4cdcd2a1c444305afcf480b39c9411734293
hmhaley <mhaley@vmgstrategies.com>
2014-12-01 12:37:22 -0800

    unknown changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,47 @@
 #   cities = City.create([{ name: 'Chicago' }: '',{ name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel': '',city: cities.first)
 
+Executive.create([{
+  username: 'sam', 
+  password_digest: 'password', 
+  strat_exec_constituent_type: 'owner',
+  prefix: 'Mr.', 
+  firstname: 'Henry',
+  lastname: 'Szeto',
+  suffix: '',
+  email: 'ucla@hotmail.com',
+  mobile_tel: '310-999-9999',
+  mobile_carrier: 'att',
+  office_tel: '888-888-8888',
+  social_twitter: 'hszeto',
+  social_linkedin: 'http://www.linkedin.com/pub/hszeto',
+  social_blog: 'blog address',
+  is_verified_by_staff: 'not yet verified by staff',
+  is_verified_by_thirdparty: 'no third party verification',
+  exec_comments_on_self: 'henry loves his testing',
+  is_user: true
+  }]);
 
+Executive.create([{
+  username: 'henry', 
+  password_digest: 'password2', 
+  strat_exec_constituent_type: 'owner',
+  prefix: 'Mr.', 
+  firstname: 'Grant',
+  lastname: 'Alexander',
+  suffix: '',
+  email: 'grantalexander@hotmail.com',
+  mobile_tel: '310-999-9999',
+  mobile_carrier: 'att',
+  office_tel: '888-888-8888',
+  social_twitter: 'hszeto',
+  social_linkedin: 'http://www.linkedin.com/pub/hszeto',
+  social_blog: 'blog address',
+  is_verified_by_staff: 'not yet verified by staff',
+  is_verified_by_thirdparty: 'no third party verification',
+  exec_comments_on_self: 'henry loves his testing',
+  is_user: true
+  }]);
 
 Organization.create([
   
@@ -101,5 +141,5 @@ Organization.create([
   web_url: 'www.biagomedia.com', 
   sector_type: "SEO Marketing" }
   
-])
+]);
 
