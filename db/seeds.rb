6bc05456d821ae35dfc5a74c7825fce2c60f049b
hmhaley <mhaley@vmgstrategies.com>
2014-12-01 13:20:29 -0800

    heroku3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }: '',{ name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel': '',city: cities.first)
 
-<<<<<<< HEAD
-document.get
-=======
->>>>>>> master
 
 
 Organization.create([
