82dce9785961117d662cc0d1c4e84ee8569b3b25
hmhaley <mhaley@vmgstrategies.com>
2014-12-01 15:29:07 -0800

    added fields for latitude and longitude on the organizations model schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119210209) do
+ActiveRecord::Schema.define(version: 20141201232722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 20141119210209) do
     t.string   "certifications"
     t.string   "source_original"
     t.string   "is_active"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "relationships", id: false, force: true do |t|
