2655c8167d7786b44b2dee295bb1a27955637b5c
Henry Szeto <ucla@hotmail.com>
2014-12-01 20:56:46 -0800

    Google map working on front page

--- app/assets/javascripts/ng-app/controllers/mapController.js
+++ app/assets/javascripts/ng-app/controllers/mapController.js
@@ -4,14 +4,12 @@ angular.module('app')
 
  var org_endpoint = 'api/organizations';
 
-  
-
   api.fetchAll('api/organizations')
   .then(function(data){
     // console.log(data.data[0].image);
     $scope.address_array = [];
     for (var i = 0; i < data.data.length; i++){
-      $scope.address_array.push([data.data[i].address_street + ", " + data.data[i].hq_address_city + ", " + "CA" + ", " + data.data[i].hq_address_zip ]);
+      $scope.address_array.push([data.data[i].latitude, data.data[i].longitude]);
     }
 
      console.log($scope.address_array);
