eaa5e08133d218b7184ff5a06fdc7343826fd70d
hmhaley <mhaley@vmgstrategies.com>
2014-12-01 18:58:26 -0800

    took out other username length validation

--- app/models/executive.rb
+++ app/models/executive.rb
@@ -4,7 +4,7 @@ include ActiveModel::SecurePassword
 	validates_uniqueness_of :email
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates_length_of :email, maximum: 255
-	validates_length_of :username, minimum: 4, maximum: 20
+	# validates_length_of :username, minimum: 4, maximum: 20
 	validates_length_of :password, minimum: 4, maximum: 20
 	#validates_length_of :password_digest, minimum: 4, maximum: 20
 	validates_presence_of :firstname
